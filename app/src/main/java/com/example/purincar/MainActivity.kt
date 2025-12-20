0069eac690862ae91d91a43e4efaf74526c109f8
donovanchen <donovanachen@gmail.com>
2025-12-20 11:22:06 +0100

    home screen with add button

--- app/src/main/java/com/example/purincar/MainActivity.kt
+++ app/src/main/java/com/example/purincar/MainActivity.kt
@@ -7,15 +7,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.purincar.ui.theme.PurinCarTheme
-import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,10 +43,25 @@ class MainActivity : ComponentActivity() {
                     Box(
                         modifier = Modifier
                             .fillMaxSize()
-                            .padding(16.dp),
-                        contentAlignment = Alignment.Center
+                            .padding(48.dp),
                     ) {
-                        Text(text = "Please enter a car")
+                        Text(
+                            text = "Please enter a car",
+                            textAlign = TextAlign.Center,
+                            fontSize = 24.sp,
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+
+                        FloatingActionButton(
+                            onClick = ({ cars = cars + listOf("Toyota") }),
+                            modifier = Modifier.align(Alignment.BottomEnd),
+                            shape = CircleShape
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Add,
+                                contentDescription = "Add a car",
+                            )
+                        }
                     }
                 }
             }
