27dd8a10969bc6fca526d9ef8d65ffe30c0a0303
donovanchen <donovanachen@gmail.com>
2026-01-13 18:23:44 -0800

    smartcar api

--- app/src/main/java/com/example/purincar/MainActivity.kt
+++ app/src/main/java/com/example/purincar/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.purincar
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import com.example.purincar.data.PurinCarDatabase
 import com.smartcar.sdk.SmartcarAuth
 import com.smartcar.sdk.SmartcarCallback
@@ -14,6 +16,7 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var smartcarAuth: SmartcarAuth
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
